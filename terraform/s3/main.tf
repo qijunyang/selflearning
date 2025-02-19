41983677306c93a8f31573c929673dad7b04256c
Qijun Yang <qijun.yang@morningstar.com>
2025-02-18 20:23:34 -0600

    add s3 and cloudfront

--- terraform/s3/main.tf
+++ terraform/s3/main.tf
@@ -25,3 +25,14 @@ resource "aws_s3_bucket" "example" {
     FUNCTION    = "app"
   }
 }
+
+resource "aws_s3_object" "object" {
+  bucket = aws_s3_bucket.example.id
+  key    = "index.html"
+  source = "${path.module}/index.html"
+
+  # The filemd5() function is available in Terraform 0.11.12 and later
+  # For Terraform 0.11.11 and earlier, use the md5() function and the file() function:
+  # etag = "${md5(file("path/to/file"))}"
+  etag = filemd5("${path.module}/index.html")
+}
